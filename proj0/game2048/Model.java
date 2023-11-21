442482b6a4d0381e67da41c85925f78dd17bb196
Jason Wu <229126195wu@gmail.com>
2023-11-21 13:17:55 +0800

    Week 01: Proj 0 [maxTileExists]

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -155,6 +155,13 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i = i + 1) {
+            for (int j = 0; j < b.size(); j = j + 1) {
+                if (b.tile(i, j) != null && b.tile(i, j).value() == MAX_PIECE) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
